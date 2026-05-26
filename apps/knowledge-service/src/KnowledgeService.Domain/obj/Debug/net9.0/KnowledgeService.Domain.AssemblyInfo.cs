c96c18af119f510213088d22bbae78312b2bcccc
hientv2272 <hientv2272@gmail.com>
2026-05-26 14:32:24 +0700

    cm

--- apps/knowledge-service/src/KnowledgeService.Domain/obj/Debug/net9.0/KnowledgeService.Domain.AssemblyInfo.cs
+++ apps/knowledge-service/src/KnowledgeService.Domain/obj/Debug/net9.0/KnowledgeService.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KnowledgeService.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a3af19394a648a216ba3e2ee41cc7a2eff2fb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e530b312523874964edc5bc11ff34b9c14d2db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KnowledgeService.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KnowledgeService.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- apps/knowledge-service/src/KnowledgeService.Infrastructure/obj/Debug/net9.0/KnowledgeService.Infrastructure.AssemblyInfo.cs
+++ apps/knowledge-service/src/KnowledgeService.Infrastructure/obj/Debug/net9.0/KnowledgeService.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KnowledgeService.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a3af19394a648a216ba3e2ee41cc7a2eff2fb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e530b312523874964edc5bc11ff34b9c14d2db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KnowledgeService.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KnowledgeService.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
